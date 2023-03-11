4133268e99577e8ceddb11c58ad6f0c6d1eec436
Santiago Carmuega <santiago@carmuega.me>
2023-03-11 22:07:09 +0100

    feat(traverse): Expose aux data scripts (#232)

---
+++ pallas-traverse/src/aux.rs
@@ -0,0 +1,42 @@
+use std::ops::Deref;
+
+use pallas_primitives::alonzo;
+
+use crate::MultiEraTx;
+
+impl<'b> MultiEraTx<'b> {
+    pub fn aux_plutus_v1_scripts(&self) -> &[alonzo::PlutusScript] {
+        if let Some(aux_data) = self.aux_data() {
+            match aux_data.deref() {
+                alonzo::AuxiliaryData::PostAlonzo(x) => {
+                    if let Some(plutus) = &x.plutus_scripts {
+                        return plutus.as_ref();
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        &[]
+    }
+
+    pub fn aux_native_scripts(&self) -> &[alonzo::NativeScript] {
+        if let Some(aux_data) = self.aux_data() {
+            match aux_data.deref() {
+                alonzo::AuxiliaryData::PostAlonzo(x) => {
+                    if let Some(scripts) = &x.native_scripts {
+                        return scripts.as_ref();
+                    }
+                }
+                alonzo::AuxiliaryData::ShelleyMa(x) => {
+                    if let Some(scripts) = &x.auxiliary_scripts {
+                        return scripts.as_ref();
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        &[]
+    }
+}
