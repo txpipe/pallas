2d9c0f304a9bd91d8dc67ea2dbb738841a200edf
microproofs <kwhitemsg@gmail.com>
2023-04-15 00:16:45 -0400

    chore: add some more transaction builder functions. valid_until valid_after mint_assets (WIP)

--- pallas-txbuilder/src/lib.rs
+++ pallas-txbuilder/src/lib.rs
@@ -1,8 +1,16 @@
-use pallas_codec::minicbor::{self, Decode, Encode};
-use pallas_primitives::babbage::{
-    AddrKeyhash, AuxiliaryData, TransactionBody, TransactionInput, TransactionOutput, Value,
-    WitnessSet,
+use std::time::Duration;
+
+use pallas_codec::{
+    minicbor::{self, Decode, Encode},
+    utils::Bytes,
 };
+use pallas_primitives::babbage::{
+    AddrKeyhash, AuxiliaryData, PolicyId, TransactionBody, TransactionInput, TransactionOutput,
+    Value, WitnessSet,
+};
+
+// TODO: Replace with some slot conversion lookup
+const SLOT_CONVERSION: u64 = 0;
 
 #[derive(Encode, Decode, Clone)]
 pub struct Transaction {
@@ -22,11 +30,22 @@ impl Transaction {
     }
 }
 
+struct TransactionBuilderInput {
+    input: TransactionInput,
+    resolved: TransactionOutput,
+}
+
+struct TransactionBuilderOutput {
+    output: TransactionOutput,
+}
+
 pub struct TxBuilder {
-    inputs: Vec<(TransactionInput, TransactionOutput)>,
-    outputs: Vec<TransactionOutput>,
+    inputs: Vec<TransactionBuilderInput>,
+    outputs: Vec<TransactionBuilderOutput>,
     mint: Option<Value>,
     required_signers: Vec<AddrKeyhash>,
+    valid_after: Option<Duration>,
+    valid_until: Option<Duration>,
 }
 
 impl Default for TxBuilder {
@@ -43,19 +62,22 @@ impl TxBuilder {
             outputs: vec![],
             mint: None,
             required_signers: vec![],
+            valid_after: None,
+            valid_until: None,
         }
     }
 
     /// Add an input
     pub fn input(mut self, input: TransactionInput, resolved: TransactionOutput) -> Self {
-        self.inputs.push((input, resolved));
+        self.inputs
+            .push(TransactionBuilderInput { input, resolved });
 
         self
     }
 
     /// Add an output
     pub fn output(mut self, output: TransactionOutput) -> Self {
-        self.outputs.push(output);
+        self.outputs.push(TransactionBuilderOutput { output });
 
         self
     }
@@ -68,22 +90,39 @@ impl TxBuilder {
     }
 
     /// Assets to mint or burn
-    pub fn mint_assets(self) -> Self {
-        todo!()
+    pub fn mint_assets(mut self, policy: PolicyId, assets: Vec<(Bytes, u64)>) -> Self {
+        let mint = vec![(policy, assets.into())];
+
+        self.mint = Some(Value::Multiasset(0, mint.into()));
+
+        self
     }
 
-    pub fn finalize(self) -> Transaction {
+    pub fn valid_after(mut self, duration: Duration) -> Self {
+        self.valid_after = Some(duration);
+
+        self
+    }
+
+    pub fn valid_until(mut self, duration: Duration) -> Self {
+        self.valid_until = Some(duration);
+
+        self
+    }
+
+    pub fn build(self) -> Transaction {
         Transaction {
             body: TransactionBody {
-                inputs: self.inputs.into_iter().map(|i| i.0).collect(),
-                outputs: self.outputs,
+                inputs: self.inputs.into_iter().map(|i| i.input).collect(),
+                outputs: self.outputs.into_iter().map(|i| i.output).collect(),
                 fee: 0,
-                ttl: None,
+                ttl: self.valid_until.map(|i| i.as_secs() - SLOT_CONVERSION),
+
                 certificates: None,
                 withdrawals: None,
                 update: None,
                 auxiliary_data_hash: None,
-                validity_interval_start: None,
+                validity_interval_start: self.valid_after.map(|i| i.as_secs() - SLOT_CONVERSION),
                 mint: None,
                 script_data_hash: None,
                 collateral: None,
@@ -115,6 +154,8 @@ impl TxBuilder {
 #[cfg(test)]
 mod test {
 
+    use std::time::Duration;
+
     use pallas_primitives::{
         babbage::{PseudoPostAlonzoTransactionOutput, TransactionInput, TransactionOutput, Value},
         Fragment,
@@ -123,7 +164,7 @@ mod test {
     use crate::Transaction;
 
     #[test]
-    fn build() {
+    fn build_basic() {
         let input = TransactionInput {
             transaction_id: [0; 32].into(),
             index: 0,
@@ -146,10 +187,78 @@ mod test {
         let tx = Transaction::builder()
             .input(input, resolved)
             .output(output)
-            .finalize();
+            .build();
 
         let bytes = tx.encode_fragment().expect("encoding failed");
 
         assert_eq!(hex::encode(bytes), "83a300818258200000000000000000000000000000000000000000000000000000000000000000000181a20040011a000f42400200a0f5")
+    }
+
+    #[test]
+    fn build_ttl_valid_after() {
+        let input = TransactionInput {
+            transaction_id: [0; 32].into(),
+            index: 0,
+        };
+
+        let resolved = TransactionOutput::PostAlonzo(PseudoPostAlonzoTransactionOutput {
+            address: vec![].into(),
+            value: Value::Coin(1000000),
+            datum_option: None,
+            script_ref: None,
+        });
+
+        let output = TransactionOutput::PostAlonzo(PseudoPostAlonzoTransactionOutput {
+            address: vec![].into(),
+            value: Value::Coin(1000000),
+            datum_option: None,
+            script_ref: None,
+        });
+
+        let valid_after = 1618400000; // Unix time for April 14, 2021, 12:00:00 AM UTC
+        let valid_until = 1618430000;
+
+        let tx = Transaction::builder()
+            .input(input, resolved)
+            .output(output)
+            .valid_after(Duration::from_secs(valid_after))
+            .valid_until(Duration::from_secs(valid_until))
+            .build();
+
+        let bytes = tx.encode_fragment().expect("encoding failed");
+
+        assert_eq!(hex::encode(bytes), "83a500818258200000000000000000000000000000000000000000000000000000000000000000000181a20040011a000f42400200031a60774830081a6076d300a0f5")
+    }
+
+    #[test]
+    fn build_mint() {
+        let input = TransactionInput {
+            transaction_id: [0; 32].into(),
+            index: 0,
+        };
+
+        let resolved = TransactionOutput::PostAlonzo(PseudoPostAlonzoTransactionOutput {
+            address: vec![].into(),
+            value: Value::Coin(1000000),
+            datum_option: None,
+            script_ref: None,
+        });
+
+        let output = TransactionOutput::PostAlonzo(PseudoPostAlonzoTransactionOutput {
+            address: vec![].into(),
+            value: Value::Coin(1000000),
+            datum_option: None,
+            script_ref: None,
+        });
+
+        let tx = Transaction::builder()
+            .input(input, resolved)
+            .output(output)
+            .mint_assets(policy, assets)
+            .build();
+
+        let bytes = tx.encode_fragment().expect("encoding failed");
+
+        assert_eq!(hex::encode(bytes), "83a500818258200000000000000000000000000000000000000000000000000000000000000000000181a20040011a000f42400200031a60774830081a6076d300a0f5")
     }
 }
