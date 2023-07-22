fa908c768f6c70164c7254b6647cab23d0b0305c
Cainã Costa <me@cfcosta.com>
2023-07-22 12:39:21 -0300

    feat: wrap internal types for inputs and outputs

--- pallas-txbuilder/src/transaction.rs
+++ pallas-txbuilder/src/transaction.rs
@@ -1,22 +1,59 @@
 use minicbor::{Decode, Encode};
-use pallas_primitives::babbage::{AuxiliaryData, PolicyId, TransactionBody, WitnessSet};
+use pallas_codec::utils::Bytes;
+use pallas_crypto::hash::Hash;
+use pallas_primitives::babbage::{
+    AuxiliaryData, PseudoPostAlonzoTransactionOutput, TransactionBody, TransactionInput,
+    TransactionOutput, Value, WitnessSet,
+};
 
-pub enum Input {
-    Lovelaces(u64),
-    Asset(PolicyId, u64),
+#[derive(Debug, Clone)]
+pub struct Input {
+    transaction_id: Hash<32>,
+    index: u64,
 }
 
 impl Input {
-    pub fn lovelaces(amount: u64) -> Self {
-        Self::Lovelaces(amount)
+    pub fn new(transaction_id: impl Into<Hash<32>>, index: u64) -> Self {
+        Self {
+            transaction_id: transaction_id.into(),
+            index,
+        }
     }
 
-    pub fn asset(policy_id: &str, amount: u64) -> Self {
-        todo!()
+    pub fn build(self) -> TransactionInput {
+        TransactionInput {
+            transaction_id: self.transaction_id,
+            index: self.index,
+        }
     }
 }
 
-pub struct Output {}
+#[derive(Debug, Clone)]
+pub enum Output {
+    Lovelaces { address: Bytes, value: u64 },
+}
+
+impl Output {
+    pub fn lovelaces(address: impl Into<Bytes>, value: u64) -> Self {
+        Self::Lovelaces {
+            address: address.into(),
+            value,
+        }
+    }
+
+    pub fn build(self) -> TransactionOutput {
+        match self {
+            Self::Lovelaces { address, value } => {
+                TransactionOutput::PostAlonzo(PseudoPostAlonzoTransactionOutput {
+                    address,
+                    value: Value::Coin(value),
+                    datum_option: None,
+                    script_ref: None,
+                })
+            }
+        }
+    }
+}
 
 #[derive(Encode, Decode, Clone)]
 pub struct Transaction {
--- pallas-txbuilder/tests/manual.rs
+++ pallas-txbuilder/tests/manual.rs
@@ -1,36 +1,21 @@
-use pallas_primitives::babbage::{
-    PseudoPostAlonzoTransactionOutput, TransactionInput, TransactionOutput, Value,
-};
 use pallas_txbuilder::prelude::*;
 
 #[test]
-fn build_basic() {
-    let input = TransactionInput {
-        transaction_id: [0; 32].into(),
-        index: 0,
-    };
-
-    let resolved = TransactionOutput::PostAlonzo(PseudoPostAlonzoTransactionOutput {
-        address: vec![].into(),
-        value: Value::Coin(1000000),
-        datum_option: None,
-        script_ref: None,
-    });
-
-    let output = TransactionOutput::PostAlonzo(PseudoPostAlonzoTransactionOutput {
-        address: vec![].into(),
-        value: Value::Coin(1000000),
-        datum_option: None,
-        script_ref: None,
-    });
+fn test_build_manual_simplest_transaction() {
+    let input = Input::new([0; 32], 0);
+    let resolved = Output::lovelaces(vec![], 1000000).build();
+    let output = Output::lovelaces(vec![], 1000000).build();
 
     let tx = TransactionBuilder::<Manual>::new(NetworkParams::default())
-        .input(input, resolved)
+        .input(input.build(), resolved)
         .output(output)
         .build()
-        .unwrap();
+        .expect("Failed to create transaction");
 
-    let bytes = tx.encode_fragment().expect("encoding failed");
+    let expected =  "83a300818258200000000000000000000000000000000000000000000000000000000000000000000181a20040011a000f42400200a0f5";
 
-    assert_eq!(hex::encode(bytes), "83a300818258200000000000000000000000000000000000000000000000000000000000000000000181a20040011a000f42400200a0f5")
+    assert_eq!(
+        hex::encode(tx.encode_fragment().expect("encoding failed")),
+        expected
+    )
 }
