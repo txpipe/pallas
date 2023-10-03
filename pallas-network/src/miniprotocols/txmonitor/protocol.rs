a1536dfe166af3cabe8200f535296e6cd38ae647
Santiago Carmuega <santiago@carmuega.me>
2023-10-03 14:08:20 -0300

    fix(network): fix bad codec for tx monitoring replies

--- pallas-network/src/miniprotocols/txmonitor/protocol.rs
+++ pallas-network/src/miniprotocols/txmonitor/protocol.rs
@@ -1,6 +1,10 @@
+use pallas_codec::utils::{CborWrap, TagWrap};
+
 pub type Slot = u64;
 pub type TxId = String;
-pub type Tx = Vec<u8>;
+pub type Era = u8;
+pub type TxBody = pallas_codec::utils::Bytes;
+pub type Tx = (Era, TagWrap<TxBody, 24>);
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum State {
