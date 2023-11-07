ac4aef4eaadc437b500d6bfafca2a0d4c1defddf
Maico Leberle <maico.leberle@gmail.com>
2023-11-07 18:29:23 -0300

    feat(applying): validate all inputs in UTxO set (#324)

--- pallas-applying/src/byron.rs
+++ pallas-applying/src/byron.rs
@@ -1,18 +1,19 @@
 //! Utilities required for Byron-era transaction validation.
 
-use crate::types::{ByronProtParams, UTxOs, ValidationError, ValidationResult};
+use crate::types::{ByronProtParams, MultiEraInput, UTxOs, ValidationError, ValidationResult};
 
 use pallas_primitives::byron::{MintedTxPayload, Tx};
 
 // TODO: implement missing validation rules.
 pub fn validate_byron_tx(
     mtxp: &MintedTxPayload,
-    _utxos: &UTxOs,
+    utxos: &UTxOs,
     _prot_pps: &ByronProtParams,
 ) -> ValidationResult {
     let tx: &Tx = &mtxp.transaction;
     check_ins_not_empty(tx)?;
-    check_outs_not_empty(tx)
+    check_outs_not_empty(tx)?;
+    check_ins_in_utxos(tx, utxos)
 }
 
 fn check_ins_not_empty(tx: &Tx) -> ValidationResult {
@@ -25,6 +26,15 @@ fn check_ins_not_empty(tx: &Tx) -> ValidationResult {
 fn check_outs_not_empty(tx: &Tx) -> ValidationResult {
     if tx.outputs.clone().to_vec().is_empty() {
         return Err(ValidationError::TxOutsEmpty);
+    }
+    Ok(())
+}
+
+fn check_ins_in_utxos(tx: &Tx, utxos: &UTxOs) -> ValidationResult {
+    for input in tx.inputs.iter() {
+        if !(utxos.contains_key(&MultiEraInput::from_byron(input))) {
+            return Err(ValidationError::InputMissingInUTxO);
+        }
     }
     Ok(())
 }
