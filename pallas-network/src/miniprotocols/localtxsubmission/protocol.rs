b3fa40dd7a6f80b1ad83f67d5014bfdcc6bf091a
Hızır Sefa İrken <sefairken@gmail.com>
2025-03-19 18:39:23 +0300

    Use BigInt where i32 and i64 is not enough

--- pallas-network/src/miniprotocols/localtxsubmission/protocol.rs
+++ pallas-network/src/miniprotocols/localtxsubmission/protocol.rs
@@ -1,6 +1,7 @@
 use super::primitives::{Certificate, Credential, Language, StakeCredential, Voter};
 use crate::miniprotocols::localstate::queries_v16::{
-    Anchor, FieldedRewardAccount, GovAction, GovActionId, PolicyId, ProposalProcedure, ProtocolVersion, ScriptHash, TransactionInput, TransactionOutput, Value, Vote
+    Anchor, BigInt, FieldedRewardAccount, GovAction, GovActionId, PolicyId, ProposalProcedure,
+    ProtocolVersion, ScriptHash, TransactionInput, TransactionOutput, Value, Vote,
 };
 pub use crate::miniprotocols::localstate::queries_v16::{Coin, ExUnits, TaggedSet};
 use pallas_codec::minicbor::{self, Decode, Encode};
@@ -36,7 +37,7 @@ pub enum ShelleyBasedEra {
     #[n(2)]
     Allegra,
     #[n(3)]
-    Mary, 
+    Mary,
     #[n(4)]
     Alonzo,
     #[n(5)]
@@ -163,11 +164,11 @@ pub struct DisplayOSet<T>(#[n(0)] pub Set<T>);
 pub struct DisplayVotingProcedures(#[n(0)] pub VotingProcedures);
 
 #[derive(Debug, Decode, Encode, Clone, Eq, PartialEq)]
-#[cbor(flat)] 
+#[cbor(flat)]
 pub enum BabbageContextError {
     #[n(0)]
     ByronTxOutInContext(#[n(0)] TxOutSource),
-    #[n(1)] 
+    #[n(1)]
     AlonzoMissingInput(#[n(0)] TransactionInput),
     #[n(2)]
     RedeemerPointerPointsToNothing(#[n(0)] PlutusPurposeIx),
@@ -259,9 +260,7 @@ impl From<Network> for u8 {
 }
 #[derive(Debug, Decode, Encode, Clone, Eq, PartialEq)]
 #[cbor(transparent)]
-pub struct DeltaCoin(#[n(0)] pub i32);
-
-
+pub struct DeltaCoin(#[n(0)] pub BigInt);
 
 pub type Slot = u64;
 
@@ -285,7 +284,7 @@ pub enum UtxoFailure {
     #[n(2)]
     OutsideValidityIntervalUTxO(#[n(0)] ValidityInterval, #[n(1)] Slot),
     #[n(3)]
-    MaxTxSizeUTxO(#[n(0)] i64, #[n(1)] i64),
+    MaxTxSizeUTxO(#[n(0)] BigInt, #[n(1)] BigInt),
     #[n(4)]
     InputSetEmptyUTxO,
     #[n(5)]
@@ -315,7 +314,7 @@ pub enum UtxoFailure {
     #[n(17)]
     OutsideForecast(#[n(0)] Slot),
     #[n(18)]
-    TooManyCollateralInputs(#[n(0)] u16, #[n(1)] u16),
+    TooManyCollateralInputs(#[n(0)] u64, #[n(1)] u64),
     #[n(19)]
     NoCollateralInputs,
     #[n(20)]
@@ -354,7 +353,7 @@ pub struct KeyHash(#[n(0)] pub Bytes);
 pub enum ConwayUtxoWPredFailure {
     #[n(0)]
     UtxoFailure(#[n(0)] UtxoFailure),
-    #[n(1)] 
+    #[n(1)]
     InvalidWitnessesUTXOW(#[n(0)] Array<VKey>),
     #[n(2)]
     MissingVKeyWitnessesUTXOW(#[n(0)] Set<KeyHash>),
@@ -440,7 +439,7 @@ pub enum ConwayLedgerFailure {
 #[cbor(flat)]
 pub enum ConwayCertsPredFailure {
     #[n(0)]
-    WithdrawalsNotInRewardsCERTS(#[n(0)] OHashMap<FieldedRewardAccount, DisplayCoin>),  
+    WithdrawalsNotInRewardsCERTS(#[n(0)] OHashMap<FieldedRewardAccount, DisplayCoin>),
     #[n(1)]
     CertFailure(#[n(0)] ConwayCertPredFailure),
 }
