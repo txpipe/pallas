607db753e6b44b41e3de8dc90cf13e58a8cd48a5
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-08-26 15:41:24 +0200

    add kes skeleton and clean dependencies

---
+++ pallas-crypto/src/kes/mod.rs
@@ -0,0 +1,49 @@
+#![warn(missing_docs, rust_2018_idioms)]
+//! A key evolving signatures implementation based on
+//! "Composition and Efficiency Tradeoffs for Forward-Secure Digital Signatures"
+//! by Tal Malkin, Daniele Micciancio and Sara Miner
+//! <https://eprint.iacr.org/2001/034>
+//!
+//! Specfically we do the binary sum composition directly as in the paper, and
+//! then use that in a nested\/recursive fashion to construct up to a 7-level
+//! deep binary tree version.
+//!
+//! We provide two different implementations in this crate, to provide compatibility
+//! with Cardano's different eras. The first, `SumKes`, is a trivial construction,
+//! while the second, `SumCompactKes`, is a version with a more compact signature.
+//!
+//! Consider the following Merkle tree:
+//!
+//! ```ascii
+//!       (A)
+//!      /   \
+//!   (B)     (C)
+//!   / \     / \
+//! (D) (E) (F) (G)
+//!      ^
+//!  0   1   2   3
+//! ```
+//!
+//! The caret points at leaf node E, indicating that the current period is 1.
+//! The signatures for leaf nodes D through G all contain their respective
+//! DSIGN keys.
+//!
+//! In the naive `SumKes` signatures the signature for branch node B holds
+//! the signature for node E, and the VerKeys for nodes D and E. The signature
+//! for branch node A (the root node), the signature for node B and the
+//! VerKeys for nodes B and C. In other words, the number of individual hashes
+//! to be stored equals the depth of the Merkle tree.
+//!
+//! Instead, the more efficient `SumCompactKes` gets rid of some redundant data
+//! of the signature. In particular, the signature for branch node B only holds
+//! the signature for node E, and the VerKey for node D. It can reconstruct its
+//! own VerKey from these two. The signature for branch node A (the root node),
+//! then, only contains the VerKey for node C, and the signature for node B. In
+//! other words, the number of individual hashes to be stored equals the depth
+//! of the Merkle tree.
+
+pub mod common;
+pub mod errors;
+pub mod traits;
+
+pub use common::PublicKey;
---
+++ pallas-crypto/src/kes/traits.rs
@@ -0,0 +1,78 @@
+//! Traits that define a KES signature instance
+use crate::kes::common::PublicKey;
+use crate::kes::errors::Error;
+
+/// Trait that defined a Kes secret key
+pub trait KesSk<'a>: Sized {
+    /// Type of the associated signature
+    type Sig;
+    /// Size of SK
+    const SIZE: usize;
+
+    /// Key generation
+    fn keygen(key_buffer: &'a mut [u8], seed: &'a mut [u8]) -> (Self, PublicKey);
+
+    /// KES signature, using `self`.
+    fn sign(&self, m: &[u8]) -> Self::Sig;
+
+    /// Update key by taking a mutable reference to `self`
+    fn update(&mut self) -> Result<(), Error>;
+
+    /// Return the current period of the secret key
+    fn get_period(&self) -> u32;
+
+    /// Convert the slice of bytes into `Self`.
+    ///
+    /// # Errors
+    /// The function fails if
+    /// * `bytes.len()` is not of the expected size
+    fn from_bytes(bytes: &'a mut [u8]) -> Result<Self, Error>;
+
+    /// Convert `Self` into it's byte representation. In particular, the encoding returns
+    /// the following array of size `Self::SIZE + 4`:
+    /// ( sk_{-1} || seed || self.lhs_pk || self.rhs_pk || period )
+    /// where `sk_{-1}` is the secret secret key of lower depth.
+    /// Note that the period is only included in the last layer.
+    fn as_bytes(&self) -> &[u8];
+}
+
+/// Trait that defines a KES signature
+///
+/// # Example
+/// ```
+/// use pallas_crypto::kes::Sum6Kes;
+/// use pallas_crypto::kes::traits::{KesSig, KesSk};
+/// // The function caller needs to allocate memory for the secret key
+/// let mut key_buffer = [0u8; Sum6Kes::SIZE + 4];
+/// let mut seed = [0u8; 32];
+/// let (mut skey, pkey) = Sum6Kes::keygen(&mut key_buffer, &mut seed);
+/// let dummy_message = b"tilin";
+/// let sigma = skey.sign(dummy_message);
+///
+/// assert!(sigma.verify(0, &pkey, dummy_message).is_ok());
+///
+/// // Key can be updated 63 times
+/// for _ in 0..63 {
+///     assert!(skey.update().is_ok());
+/// }
+/// ```
+pub trait KesSig: Sized {
+    /// Verify the signature
+    fn verify(&self, period: u32, pk: &PublicKey, m: &[u8]) -> Result<(), Error>;
+}
+
+/// Trait that defined a CompactKES signature. Instead of recursively verifying, we simply
+/// verify once (equality with the root), and else we recompute the root of the subtree.
+/// When we reach the leaf, we also verify the ed25519 signature.
+pub trait KesCompactSig: Sized {
+    /// Verify the root equality
+    fn verify(&self, period: u32, pk: &PublicKey, m: &[u8]) -> Result<(), Error> {
+        let pk_subtree = self.recompute(period, m)?;
+        if pk == &pk_subtree {
+            return Ok(());
+        }
+        Err(Error::InvalidHashComparison)
+    }
+    /// Recompute the root of the subtree, and verify ed25519 if on leaf
+    fn recompute(&self, period: u32, m: &[u8]) -> Result<PublicKey, Error>;
+}
