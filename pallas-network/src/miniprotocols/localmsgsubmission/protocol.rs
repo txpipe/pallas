63461e8b7050a2478d083ab5770c94471917223a
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-09-02 12:38:37 +0200

    refactor(dmq): update 'DmqMsg' structure

--- pallas-network/src/miniprotocols/localmsgsubmission/protocol.rs
+++ pallas-network/src/miniprotocols/localmsgsubmission/protocol.rs
@@ -7,20 +7,20 @@ pub struct DmqMsg {
     /// The message body.
     pub msg_body: Vec<u8>,
 
-    /// The block number at which the message is to be submitted.
-    pub block_number: u32,
-
-    /// The time to live for the message in number of blocks.
-    pub ttl: u16,
-
     /// The KES signature of the message created by the SPO sending the message.
     pub kes_signature: Vec<u8>,
+
+    /// The KES period at which the KES signature was created.
+    pub kes_period: u64,
 
     /// The operational certificate of the SPO that created the message.
     pub operational_certificate: Vec<u8>,
 
-    /// The KES period at which the KES signature was created.
-    pub kes_period: u32,
+    /// The cold verification key of the SPO that created the message.
+    pub cold_verification_key: Vec<u8>,
+
+    /// The expiration timestamp of the message.
+    pub expires_at: u32,
 }
 
 /// Reject reason.
