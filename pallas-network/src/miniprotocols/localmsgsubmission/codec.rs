d34b143f838c16e33759d63cd3aec8eaab539111
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-09-20 01:21:10 +0200

    refactor(network): update DMQ message to match CIP (#696)

--- pallas-network/src/miniprotocols/localmsgsubmission/codec.rs
+++ pallas-network/src/miniprotocols/localmsgsubmission/codec.rs
@@ -1,25 +1,19 @@
 use pallas_codec::minicbor::{decode, encode, Decode, Decoder, Encode, Encoder};
 
-use super::{DmqMsg, DmqMsgValidationError};
+use super::{DmqMsg, DmqMsgPayload, DmqMsgValidationError};
 
 impl<'b> Decode<'b, ()> for DmqMsg {
     fn decode(d: &mut Decoder<'b>, _ctx: &mut ()) -> Result<Self, decode::Error> {
         d.array()?;
-        let msg_id = d.bytes()?.to_vec();
-        let msg_body = d.bytes()?.to_vec();
-        let block_number = d.u32()?;
-        let ttl = d.u16()?;
+        let payload = DmqMsgPayload::decode(d, _ctx)?;
         let kes_signature = d.bytes()?.to_vec();
         let operational_certificate = d.bytes()?.to_vec();
-        let kes_period = d.u32()?;
+        let cold_verification_key = d.bytes()?.to_vec();
         Ok(DmqMsg {
-            msg_id,
-            msg_body,
-            block_number,
-            ttl,
+            msg_payload: payload,
             kes_signature,
             operational_certificate,
-            kes_period,
+            cold_verification_key,
         })
     }
 }
@@ -30,14 +24,43 @@ impl Encode<()> for DmqMsg {
         e: &mut Encoder<W>,
         _ctx: &mut (),
     ) -> Result<(), encode::Error<W::Error>> {
+        e.array(2)?;
+        e.encode(&self.msg_payload)?;
+        e.bytes(&self.kes_signature)?;
+        e.bytes(&self.operational_certificate)?;
+        e.bytes(&self.cold_verification_key)?;
+
+        Ok(())
+    }
+}
+
+impl<'b> Decode<'b, ()> for DmqMsgPayload {
+    fn decode(d: &mut Decoder<'b>, _ctx: &mut ()) -> Result<Self, decode::Error> {
+        d.array()?;
+        let msg_id = d.bytes()?.to_vec();
+        let msg_body = d.bytes()?.to_vec();
+        let kes_period = d.u64()?;
+        let expires_at = d.u32()?;
+        Ok(DmqMsgPayload {
+            msg_id,
+            msg_body,
+            kes_period,
+            expires_at,
+        })
+    }
+}
+
+impl Encode<()> for DmqMsgPayload {
+    fn encode<W: encode::Write>(
+        &self,
+        e: &mut Encoder<W>,
+        _ctx: &mut (),
+    ) -> Result<(), encode::Error<W::Error>> {
         e.array(6)?;
         e.bytes(&self.msg_id)?;
         e.bytes(&self.msg_body)?;
-        e.u32(self.block_number)?;
-        e.u16(self.ttl)?;
-        e.bytes(&self.kes_signature)?;
-        e.bytes(&self.operational_certificate)?;
-        e.u32(self.kes_period)?;
+        e.u64(self.kes_period)?;
+        e.u32(self.expires_at)?;
 
         Ok(())
     }
