14baceb733bd647ac51c904380f2bb2ad62d928d
etorreborre <etorreborre@yahoo.com>
2025-12-03 10:13:14 +0100

    feat: add some Clone instances for Messages

--- pallas-network/src/miniprotocols/txsubmission/protocol.rs
+++ pallas-network/src/miniprotocols/txsubmission/protocol.rs
@@ -26,7 +26,7 @@ pub struct EraTxId(pub u16, pub Vec<u8>);
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct EraTxBody(pub u16, pub Vec<u8>);
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TxIdAndSize<TxID>(pub TxID, pub TxSizeInBytes);
 
 #[derive(Error, Debug)]
@@ -50,7 +50,7 @@ pub enum Error {
     Plexer(multiplexer::Error),
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Message<TxId, TxBody> {
     Init,
     RequestTxIds(Blocking, TxCount, TxCount),
